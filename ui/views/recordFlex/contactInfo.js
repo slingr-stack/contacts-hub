1904a69fa0d98d1a5a9aec040b8eeca286c3fd12
smoyano <smoyano@slingr.io>
2024-06-12 17:18:01 -0300

    Pagination settings

--- ui/views/recordFlex/contactInfo.js
+++ ui/views/recordFlex/contactInfo.js
@@ -6,20 +6,39 @@ manually unless you know what you are doing. To be safe, only modify the scripts
 
 function notesTableRowsCalculation(record, options) {
     let rows = [];
-    record.field('notes').forEach(function (note) {
+    let filters = options.filters || {};
+    let notes = record.field('notes').val();
+    let from = options.from || 0;
+    let to = options.to || notes.length;
+    // apply filtering based on the 'note' column
+    notes = notes.filter(function(noteItem) {
+        let noteValue = noteItem.note
+        if (filters.note && noteValue.toLowerCase().includes(filters.note.toLowerCase())) {
+            return true;
+        }
+        return !filters.note;
+    });
+    // retrieve only the requested batch
+    let notesBatch = notes.slice(from, to);
+    notesBatch.forEach(function (note) {
+        let userField = note.field('user');
+        let noteField = note.field('note');
         rows.push({
-            user: note.field('user').label(),
-            note: note.field('note').val()
+            user: userField.label(),
+            note: noteField.val()
         });
     });
     let columns = [
         {
-            "name": "user",
-            "label": "User"
+            name: "user",
+            label: "User"
         },
         {
-            "name": "note",
-            "label": "Note"
+            name: "note",
+            label: "Note",
+            options: {
+                allowFiltering: true
+            }
         }
     ];
     return {
